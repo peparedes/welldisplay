7ab727b14f0609859e07a9194d09b06ded2977ba
Peter Washington <washingtonp@google.com>
2018-05-28 23:03:49 -0700

    Example of how we can have exact precise timing of interventions

--- nshift/main.m
+++ nshift/main.m
@@ -34,28 +34,55 @@ int main(int argc, const char * argv[]) {
     CBBlueLightClient *client = [[CBBlueLightClient alloc] init];
     int time = 0;
     
+    // Define time to wait.
+    uint64_t time_to_wait_subliminal = 1;
+    uint64_t time_to_wait_10 = 10 * NANOS_PER_SEC;
+
+    // Set background screen to green.
+    [MacGammaController setGammaWithRed:0 green:10 blue:0];
     
-    while (TRUE) {
-        time++;
-        if (time % 2 == 1) {
-            //NSLog(@"ON");
-            [MacGammaController setGammaWithRed:0 green:10 blue:0];
-        }
-        else {
-            //NSLog(@"OFF");
-            CGDisplayRestoreColorSyncSettings();
-        }
-        
-        // Wait for next time.
-        mach_timebase_info(&timebase_info);
-        uint64_t time_to_wait = nanos_to_abs(1ULL * NANOS_PER_SEC);
-        uint64_t now = mach_absolute_time();
-        mach_wait_until(now + time_to_wait);
-    }
+    // Wait until resetting settings.
+    mach_timebase_info(&timebase_info);
+    uint64_t now = mach_absolute_time();
+    mach_wait_until(now + time_to_wait_subliminal);
     
-    int userInput;
-    scanf("%i", &userInput);
-    NSLog(@"You typed %i.", userInput);
+    // Restore color settings.
+    CGDisplayRestoreColorSyncSettings();
+    
+    // Wait for 10 seconds.
+    mach_timebase_info(&timebase_info);
+    uint64_t now2 = mach_absolute_time();
+    mach_wait_until(now2 + time_to_wait_10);
+    
+    // Set background screen to green.
+    [MacGammaController setGammaWithRed:0 green:10 blue:0];
+    
+    // Wait for 10 seconds.
+    mach_timebase_info(&timebase_info);
+    uint64_t now3 = mach_absolute_time();
+    mach_wait_until(now3 + time_to_wait_10);
+    
+    // Restore color settings.
+    CGDisplayRestoreColorSyncSettings();
+    
+    
+    // SUBLIMINAL INTERVENTION EXAMPLE:::
+    
+    // Wait for 10 seconds.
+    mach_timebase_info(&timebase_info);
+    uint64_t now4 = mach_absolute_time();
+    mach_wait_until(now4 + time_to_wait_10);
+    
+    // Set background screen to green.
+    [MacGammaController setGammaWithRed:0 green:10 blue:0];
+    
+    // Super quick waiting.
+    mach_timebase_info(&timebase_info);
+    uint64_t now5 = mach_absolute_time();
+    mach_wait_until(now + time_to_wait_subliminal);
+
+    // Restore color settings.
+    CGDisplayRestoreColorSyncSettings();
     
     return 0;
 }
