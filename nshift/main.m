454b0df03b385209063e99e2026561e9d33eb2bd
Peter Washington <washingtonp@google.com>
2018-05-28 22:48:32 -0700

    Add mach timing for more precise timing/scheduling

--- nshift/main.m
+++ nshift/main.m
@@ -8,48 +8,34 @@
 #import "CBBlueLightClient.h"
 #import "MacGammaController.h"
 
+#include <mach/mach.h>
+#include <mach/mach_time.h>
+
 #define RATE_OF_CHANGE 0.001
 
-@interface SubliminalScheduler : NSObject
-- (void)subliminalLoop;
-- (void)targetMethod;
-@end
 
-@implementation SubliminalScheduler
+static const uint64_t NANOS_PER_USEC = 1000ULL;
+static const uint64_t NANOS_PER_MILLISEC = 1000ULL * NANOS_PER_USEC;
+static const uint64_t NANOS_PER_SEC = 1000ULL * NANOS_PER_MILLISEC;
 
-- (void)subliminalLoop {
-    NSLog(@"Hello, World!");
-    
-    [NSTimer scheduledTimerWithTimeInterval:2.0
-                                     target:self
-                                     selector:@selector(targetMethod)
-                                     userInfo:nil
-                                     repeats:YES];
+static mach_timebase_info_data_t timebase_info;
+
+static uint64_t abs_to_nanos(uint64_t abs) {
+    return abs * timebase_info.numer  / timebase_info.denom;
 }
 
-- (void)targetMethod {
-    NSLog(@"Hello, World!");
+static uint64_t nanos_to_abs(uint64_t nanos) {
+    return nanos * timebase_info.denom / timebase_info.numer;
 }
-
-@end
-
 
 
 int main(int argc, const char * argv[]) {
-    float strength = 0;
-    int shouldIncrease = 0;
-    
-    SubliminalScheduler *subliminalScheduler = [SubliminalScheduler alloc];
-    subliminalScheduler.subliminalLoop;
-
-    
     
     CBBlueLightClient *client = [[CBBlueLightClient alloc] init];
-    
     int time = 0;
-
-    /*while (TRUE) {
-        //NSLog(@"%d",time);
+    
+    
+    while (TRUE) {
         time++;
         if (time % 2 == 1) {
             //NSLog(@"ON");
@@ -59,7 +45,13 @@ int main(int argc, const char * argv[]) {
             //NSLog(@"OFF");
             CGDisplayRestoreColorSyncSettings();
         }
-    }*/
+        
+        // Wait for next time.
+        mach_timebase_info(&timebase_info);
+        uint64_t time_to_wait = nanos_to_abs(1ULL * NANOS_PER_SEC);
+        uint64_t now = mach_absolute_time();
+        mach_wait_until(now + time_to_wait);
+    }
     
     int userInput;
     scanf("%i", &userInput);
